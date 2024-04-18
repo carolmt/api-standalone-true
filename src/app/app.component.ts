67b638ee8092e875f0aacc4e71320fe93a4da7b4
Carolina Maldonado Torres <carolinamaldonado12@hotmail.es>
2024-04-18 10:37:18 +0200

    fetch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +8,44 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  
   title = 'apis';
+  URL_BASE ='https://jsonplaceholder.typicode.com/posts';
+
+  ngOnInit(): void {
+    console.log('Inicio del proyecto');
+    // this.getPosts();
+    // this.createPost();
+    // this.deletePost();
+  }
+
+  getPosts() {
+    fetch(this.URL_BASE + "/1")
+    .then((response) => console.log(response.json))
+    .then((json) => console.log(json));
+  }
+
+  createPost() {
+    fetch(this.URL_BASE, {
+      method: 'POST',
+      body: JSON.stringify({
+        title: 'foo',
+        body: 'bar',
+        userId: 1,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+  }
+
+  deletePost(){
+    fetch(this.URL_BASE + "/1", {
+    method: 'DELETE',
+    });
+  }
+
 }
